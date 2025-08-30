695953cb2bc0eee956e0594f357dbb0d4c846827
Sameer Nadeem <116316998+SameerNadeem@users.noreply.github.com>
2025-08-29 17:22:05 -0700

    feat: Implement Layout component with Bootstrap navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import CarDetail from './pages/CarDetail';
 import 'bootstrap/dist/css/bootstrap.min.css';
